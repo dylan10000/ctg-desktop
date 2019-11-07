e65cee285837a109c2ff3b970fe859bfcd13611f
644328378@qq.com <>
2019-11-07 10:32:21 +0800

    上传代码到分支[master], recordId=68

---
+++ src/main/java/com/ctgdesktop/desktop/dao/IUserDao.java
@@ -0,0 +1,15 @@
+package com.ctgdesktop.desktop.dao;
+
+import com.ctgdesktop.desktop.entity.User;
+import com.ctgdesktop.desktop.mapper.UserMapper;
+import org.apache.ibatis.annotations.Param;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository("userDao")
+public interface IUserDao extends UserMapper {
+    List<User> findByName(@Param("userName") String username);
+
+    User selectById(@Param("id") Integer id);
+}
