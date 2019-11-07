5a1bd89ef825b784afde040704edd08f3f2eec4c
chenjianhui <chenjianhui@chinatelecom.cn>
2019-11-07 11:13:24 +0800

    增加默认控制器

---
+++ src/main/java/com/ctgdesktop/desktop/controller/DefaultController.java
@@ -0,0 +1,14 @@
+package com.ctgdesktop.desktop.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class DefaultController {
+    @RequestMapping("/hi")
+    public String sayHi(){
+        return "hello ctg";
+    }
+
+
+}
